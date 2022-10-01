5849c433e094904960e7555fd8ea87748b49861a
Erick <erickquintao@outlook.com>
2022-10-01 17:44:35 -0300

    ok

---
+++ Exercicios/Pasta-trybe/back-end/bloco-20-sql/dia-3/exe-1.sql
@@ -0,0 +1,22 @@
+-- SELECT first_name,active  FROM sakila.customer
+-- WHERE active IS FAlSE AND
+-- store_id = 2 AND first_name <> 'KENNETH' ORDER BY first_name;
+
+-- SELECT title,description,release_year,replacement_cost FROM sakila.film
+-- WHERE replacement_cost > 18 
+--  ORDER BY replacement_cost DESC,title LIMIT 100;
+
+--  SELECT COUNT(first_name) FROM sakila.customer
+--  WHERE store_id = 1  AND active  IS TRUE 
+-- ;
+
+-- SELECT first_name FROM sakila.customer
+-- WHERE active IS FALSE AND store_id = 1;
+
+-- SELECT title FROM sakila.film
+-- WHERE rating = 'NC-17' ORDER BY rental_rate DESC ,title
+-- LIMIT 50;
+
+
+
+ 
---
+++ Exercicios/Pasta-trybe/back-end/bloco-20-sql/dia-3/exe-2.sql
@@ -0,0 +1,18 @@
+-- SELECT * FROM sakila.film
+-- WHERE title LIKE '%ace%';
+
+-- SELECT * FROM sakila.film
+-- WHERE title LIKE '%china';
+
+-- USE sakila;
+-- SELECT * FROM film
+-- WHERE description LIKE '%girl%' AND title LIKE '%lord';
+
+-- SELECT * FROM sakila.film
+-- WHERE title LIKE '___gon%' AND description LIKE '%Documentary%';
+
+-- SELECT * FROM sakila.film
+-- WHERE title LIKE '%academy' OR title LIKE 'mosquito%' LIMIT 2;
+
+SELECT * FROM sakila.film
+WHERE description LIKE '%monkey%' AND description LIKE '%sumo%';
